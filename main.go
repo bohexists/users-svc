7f7d780c15b1ae2626dfb462bada48a466e804ae
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-07 21:48:40 +0100

    Finished radius function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -11,16 +12,24 @@ func main() {
 	})
 
 	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, r.URL.Query().Get("massage"))
+		fmt.Fprint(w, r.URL.Query().Get("message"))
 	})
 
 	http.HandleFunc("/circle", func(w http.ResponseWriter, r *http.Request) {
 		radius := r.URL.Query().Get("radius")
 		if radius == "" {
-			http.Error(w, "Параметр 'radius' отсутствует", http.StatusBadRequest)
+			http.Error(w, "Параметр radius не передан", http.StatusBadRequest)
 			return
 		}
-		fmt.Fprintf(w, "Радиус: %s", radius)
+
+		radiusFloat, err := strconv.ParseFloat(radius, 32)
+		if err != nil {
+			http.Error(w, "Неверное значение radius", http.StatusBadRequest)
+			return
+		}
+
+		area := 3.14159 * radiusFloat * radiusFloat
+		fmt.Fprintf(w, "Радиус: %.2f, Площадь: %.2f", radiusFloat, area)
 	})
 
 	http.ListenAndServe(":80", nil)
