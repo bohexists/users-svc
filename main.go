6df3e6a140e192f8699580b897925807c3075972
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-09 09:07:41 +0100

    initial version of REST API

--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
+	"github.com/bohexists/cache-library/cache"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Привет")
-	})
+type CacheStorage struct {
+	cache *cache.Cache
+}
 
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, r.URL.Query().Get("message"))
-	})
-
-	http.HandleFunc("/circle", func(w http.ResponseWriter, r *http.Request) {
-		radius := r.URL.Query().Get("radius")
-		if radius == "" {
-			http.Error(w, "Параметр radius не передан", http.StatusBadRequest)
-			return
-		}
-
-		radiusFloat, err := strconv.ParseFloat(radius, 32)
-		if err != nil {
-			http.Error(w, "Неверное значение radius", http.StatusBadRequest)
-			return
-		}
-
-		area := 3.14159 * radiusFloat * radiusFloat
-		fmt.Fprintf(w, "Радиус: %.2f, Площадь: %.2f", radiusFloat, area)
-	})
-
-	http.ListenAndServe(":80", nil)
+func NewCacheStorage() *CacheStorage {
+	return &CacheStorage{
+		cache: cache.New(cache.CacheConfig{MaxSize: 100, DefaultTTL: 0}),
+	}
 }
---
+++ storage.go
@@ -0,0 +1,9 @@
+package main
+
+type User struct {
+	ID         string `json:"id"`
+	FirstName  string `json:"first_name"`
+	SecondName string `json:"second_name"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+}
