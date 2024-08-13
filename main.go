15b57f6000736ec2ffe986bb0aa09e0f0595b3c5
Bohdan Kyrylenko <kyrylenko.bohdan@gmail.com>
2024-08-13 20:02:00 +0100

    Added /user/search metjod

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	r.PUT("/user/:id", updateUserHandler(storage))
 	r.DELETE("/user/:id", deleteUserHandler(storage))
 	r.GET("/users", getAllUsersHandler(storage))
-	// Start the server
+	r.GET("/user/search", searchUserByEmailHandler(storage))
+	// Start the servergit a
 	r.Run(":8080")
 }
